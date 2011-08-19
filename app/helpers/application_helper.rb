d63ebc900567d9b510475f2bd4af6dab7c9b6cb0
Nick DeSteffen <nick.desteffen@gmail.com>
2011-08-18 22:45:07 -0500

    comment refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,7 @@ module ApplicationHelper
   def error_messages_for(object)
     render :partial => "shared/error_messages", :object => object
   end
-  
-  def gravatar_image(gravatar_hash, options={})
-    return if gravatar_hash.blank?
-    size = options[:size] || 75
-    alt = options[:alt] || ""
-    title = options[:title] || ""
-    image_tag("http://www.gravatar.com/avatar/#{gravatar_hash}?size=#{size}", :alt => alt, :title => title)
-  end
-  
+    
   def format_timestamp(timestamp)
     return if timestamp.blank?
     timestamp.strftime("%m/%d/%Y %I:%M%p").downcase
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+  
+  def gravatar_image(gravatar_hash, options={})
+    return if gravatar_hash.blank?
+    size = options[:size] || 75
+    alt = options[:alt] || ""
+    title = options[:title] || ""
+    image_tag("http://www.gravatar.com/avatar/#{gravatar_hash}?size=#{size}", :alt => alt, :title => title)
+  end
+  
+  def comment_name(comment)
+    if comment.url.present?
+      link_to comment.name, comment.url, :target => "_blank"
+    else
+      comment.name
+    end
+  end
+  
 end
