0251f0cdd02a4b690c29431f380635fce1203ff8
Nick DeSteffen <nick.desteffen@gmail.com>
2011-10-04 19:55:01 -0500

    Setting sass config

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module PersonalWebsite
     # Enable the asset pipeline
     config.assets.enabled = true
     
+    config.sass.preferred_syntax = :sass
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
