d73b85e60dd216d7aa10a5a708945a1ed4eab095
Nick DeSteffen <nick.desteffen@gmail.com>
2011-12-24 17:39:41 -0600

    Fixing incorrectly named comment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ PersonalWebsite::Application.config.session_store :cookie_store, key: '_personal
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Railsthreetwo::Application.config.session_store :active_record_store
+# PersonalWebsite::Application.config.session_store :active_record_store
