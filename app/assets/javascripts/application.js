e556ea0a5569b3e8670f30154d74d253c1947343
Nick DeSteffen <nick.desteffen@gmail.com>
2012-06-30 21:38:26 -0500

    Adding jquery-ujs javascript back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 // the compiled file.
 //
 //= require jquery
+//= require jquery_ujs
 //= require syntaxhighlighter_3.0.83/scripts/shCore
 //= require syntaxhighlighter_3.0.83/scripts/shBrushRuby
 //= require syntaxhighlighter_3.0.83/scripts/shBrushJScript
