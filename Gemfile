8c22a0d3c406b13ff891747284034018a0cc2e7d
Nick DeSteffen <nick.desteffen@gmail.com>
2012-07-11 23:17:27 -0500

    Updating rspec

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'friendly_id', "4.0.7"
 gem 'bcrypt-ruby', '3.0.1'
 gem 'redcarpet', '2.1.1'
 gem 'exception_notification', '2.6.1'
-gem 'thin', '1.3.1'
+gem 'thin', '1.4.1'
 gem 'rakismet', '1.2.1'
 
 group :assets do
@@ -17,8 +17,8 @@ group :assets do
 end
 
 group :development, :test do
-  gem "rspec-rails", "2.10.1"
-  gem "mocha", "0.11.4"
+  gem "rspec-rails", "2.11.0"
+  gem "mocha", "0.12.0", require: false
   gem "heroku"
   gem "taps"
   gem "sqlite3"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'mocha'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -11,6 +12,8 @@ RSpec.configure do |config|
   config.mock_with :mocha
   config.render_views
   config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = true
+  config.order = "random"
 
   config.before do
     Comment.any_instance.stubs(:spam?).returns(false)
