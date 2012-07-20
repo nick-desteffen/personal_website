28099652a65712f87ffe51d616f2b9d6dce8350c
Nick DeSteffen <nick.desteffen@gmail.com>
2012-07-20 16:30:57 -0500

    spec_helper cleanpup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,9 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'mocha'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
@@ -13,11 +11,12 @@ RSpec.configure do |config|
   config.render_views
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = true
-  config.order = "random"
+  config.order = :random
 
   config.before do
     Comment.any_instance.stubs(:spam?).returns(false)
   end
+  
 end
 
 def login_as(user)
