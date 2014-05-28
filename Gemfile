29c99b885371ae472051ceef20ce5b64adfaf249
Nick DeSteffen <nick.desteffen@gmail.com>
2014-05-27 20:20:00 -0500

    Fixing resume download

--- Gemfile
+++ Gemfile
@@ -6,12 +6,12 @@ gem "pg", "0.17.1"
 gem 'jquery-rails', "3.1.0"
 gem 'friendly_id', '5.0.3'
 gem 'bcrypt', '3.1.7'
-gem 'redcarpet', '3.1.1'
+gem 'redcarpet', '3.1.2'
 gem 'pygmentize', '0.0.3'
 gem 'thin', '1.6.2'
 gem 'rakismet', '1.5.0'
 gem 'kaminari', '0.15.1'
-gem 'pg_search', "0.7.3"
+gem 'pg_search', "0.7.4"
 
 gem 'sass-rails', "4.0.3"
 gem 'coffee-rails', "4.0.1"
@@ -21,7 +21,7 @@ group :development, :test do
   gem 'rspec-rails', '3.0.0.beta2'
   gem "factory_girl_rails", "4.4.1"
   gem "byebug"
-  gem "spring", "~> 1.1.2"
+  gem "spring", "~> 1.1.3"
 end
 
 group :production do
--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,8 +1,11 @@
 class ResumeController < ApplicationController
 
   def index
-    uri = URI('https://raw.github.com/nick-desteffen/resume/master/Nicholas%20DeSteffen%20Resume.pdf')
-    resume_response = Net::HTTP.get_response(uri)
+    uri = URI('https://raw.githubusercontent.com/nick-desteffen/resume/master/Nicholas%20DeSteffen%20Resume.pdf')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    resume_request = Net::HTTP::Get.new(uri.request_uri)
+    resume_response = http.request(resume_request)
 
     send_data(resume_response.body, type: "application/pdf", filename: "nicholas_desteffen_resume.pdf")
   end
