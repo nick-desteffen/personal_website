46f615f954ebe57749c272a866f76cd57452175b
Nick DeSteffen <nick.desteffen@gmail.com>
2014-06-14 07:46:57 -0500

    RSpec 3.0.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = true
   config.order = :random
+  config.infer_spec_type_from_file_location!
 
   config.before do
     allow_any_instance_of(Comment).to receive(:spam?).and_return(false)
