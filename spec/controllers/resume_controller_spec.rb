fd0e7837e8682dd5727c6d9f2b8223a2aae03351
Nick DeSteffen <nick.desteffen@gmail.com>
2014-06-14 08:50:01 -0500

    Fixing resume controller specs

--- spec/controllers/resume_controller_spec.rb
+++ spec/controllers/resume_controller_spec.rb
@@ -5,9 +5,12 @@ describe ResumeController do
   describe "index" do
     it "send the resume file from Github" do
       allow(controller).to receive(:render)
-      resume = double(:resume, body: "Nicholas DeSteffen Resume")
-      allow(Net::HTTP).to receive(:get_response).and_return(resume)
       allow(controller).to receive(:send_data).once
+
+      resume = double(:resume, body: "Nicholas DeSteffen Resume")
+      http = double(:http, :request => resume, "use_ssl=" => true)
+      allow(Net::HTTP).to receive(:new).and_return(http)
+      allow(http).to receive(:request).and_return(resume)
 
       get :index
 
