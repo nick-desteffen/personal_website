05bce820184fb9ad13ba8dc5f65d6bee2f1a7944
Nick DeSteffen <nick.desteffen@gmail.com>
2015-08-29 18:07:33 -0500

    Update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 5
 set :passenger_restart_with_sudo, true
+set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running" }
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :rails_env, :production
 
-server 'risk2210.net', user: 'nickd', roles: %w{web app db}
+server 'nickdesteffen.com', user: 'nickd', roles: %w{web app db}
